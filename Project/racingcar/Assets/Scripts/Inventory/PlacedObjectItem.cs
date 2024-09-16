2d24ff6309262cd724d43cfd9124753a4a532100
frankielp <96617665+frankielp@users.noreply.github.com>
2024-09-17 01:57:00 +0700

    del racing car

--- Project/racingcar/Assets/Scripts/Inventory/PlacedObjectItem.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class PlacedObjectItem : MonoBehaviour
-{
-    public PlayerItem PlayerItem { get; set; }
-}
--- Project/racingcar/Assets/Scripts/Settings/GlobalGameSettings.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GlobalGameSettings", menuName = "Create Game Settings", order = 0)]
-public class GlobalGameSettings : ScriptableObject 
-{
-    public Material AvailableReticleMaterial;
-
-    public Material UnavailableReticleMaterial;  
-}

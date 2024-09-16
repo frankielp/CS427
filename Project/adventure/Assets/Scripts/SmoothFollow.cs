bbe8f44c72937c2d50d195b8a422f779f002a485
frankielp <96617665+frankielp@users.noreply.github.com>
2024-09-17 01:53:57 +0700

    fix main menu bug, update version, add instruction

--- Project/adventure/Assets/Scripts/SmoothFollow.cs
+++ Project/adventure/Assets/Scripts/SmoothFollow.cs
@@ -10,20 +10,33 @@ public class SmoothFollow : MonoBehaviour
     private float height = 7.0f; // Chiều cao của camera so với đối tượng
     [SerializeField]
     private float smoothSpeed = 0.125f; // Tốc độ làm mượt di chuyển của camera
+    [SerializeField]
+    private float rotationSmoothSpeed = 5.0f; // Tốc độ làm mượt khi camera xoay
+
+    private Vector3 offset; // Khoảng cách giữa camera và đối tượng (dựa trên vị trí và hướng)
+
+    void Start()
+    {
+        // Tạo khoảng cách ban đầu giữa camera và đối tượng
+        offset = target.position - transform.position;
+    }
 
     void LateUpdate()
     {
         if (target != null)
         {
-            // Xác định vị trí mong muốn của camera
+            // Xác định vị trí mong muốn của camera dựa trên đối tượng, khoảng cách và chiều cao
             Vector3 desiredPosition = target.position - target.forward * distance + Vector3.up * height;
-            // Làm mượt vị trí của camera
+
+            // Làm mượt vị trí camera bằng cách sử dụng Vector3.Lerp
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-            // Cập nhật vị trí của camera
             transform.position = smoothedPosition;
 
-            // Camera nhìn về phía đối tượng
-            transform.LookAt(target);
+            // Tính toán hướng mà camera cần quay để luôn nhìn về phía đối tượng
+            Quaternion desiredRotation = Quaternion.LookRotation(target.position - transform.position);
+
+            // Làm mượt quá trình xoay camera
+            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, rotationSmoothSpeed * Time.deltaTime);
         }
     }
 }
---
+++ Project/adventure/Assets/Scripts/UIButtonGuide.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Open Github Button click handler.
+/// </summary>
+public class UIButtonGuide : MonoBehaviour {
+
+
+	public void showInstructionCanvas(GameObject instructionCanvas)
+	{
+		GameObject.FindWithTag("MainMenu").SetActive(false);
+		instructionCanvas.SetActive(true);
+	}
+
+}
